4e386479de3aea70d68385a693355b0555526ff4
Kamila Joseph Hernandez Vanegas <kamy0225@hotmail.com>
2024-07-23 16:35:07 -0500

    User and Authentication routes

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -4,7 +4,18 @@ export const verifyToken = async (req, res, next) => {
     try {
         let token = req.header("Authotization");
 
-    } catch (err) {
+        if (!token){
+            return res.status(403).send("Access Denied");
+        }
+
+        if (token.StartsWith("Bearer ")){
+            token = token.slice(7, token.lenght).trimleft();
+        }
+
+        const verified = jwt.verify(token, process.env.JWT_SECRET);
+        req.ser = verified;
+
+    }   catch (err) {
         res.status(500).json({ error: err.message })
     }
 }
